ae3d0c5b01eb22cf88a80aca3bb7e30944b4b50e
aliciaMartinezAsian <aliciamartinezasian@gmail.com>
2025-05-12 18:07:54 +0200

    [ADD] Login and Home navegation

---
+++ screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function LoginScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido a GoTribu</Text>
+      <Button title="Iniciar Sesión" onPress={() => navigation.replace('Home')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: 'center', alignItems: 'center',
+  },
+});
